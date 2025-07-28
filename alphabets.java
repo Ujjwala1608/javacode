82aa6e1f4fdd5617b27c1919168d3149f213b1ac
Ujjwala1608 <tejasgangarde231@gmail.com>
2025-07-28 12:44:24 -0700

    alphabets

---
+++ alphabets.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class alphabets{
+    public static void main(String args[]){
+        for(char c='A';c<='Z';c++){
+            System.out.print(c+" ");
+        }
+    }
+}
